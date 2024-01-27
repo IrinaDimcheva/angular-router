c2a8df659b065a45cbf3a2066bd992e68514802c
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-28 00:28:12 +0200

    Add root and page not found routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
   {
     path: 'courses',
     loadChildren: () =>
@@ -11,6 +13,7 @@ const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
