ce00b956e0d0d5cab60cfa3017edbe47fd8cf853
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-28 13:40:17 +0200

    Add course child routes

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { courseResolver } from './services/course.resolver';
+import { LessonsListComponent } from './lessons-list/lessons-list.component';
+import { LessonDetailComponent } from './lesson/lesson-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: ':courseUrl',
     component: CourseComponent,
+    children: [
+      { path: '', component: LessonsListComponent },
+      { path: 'lessons/:lessonSeqNo', component: LessonDetailComponent },
+    ],
     resolve: { course: courseResolver },
   },
 ];
