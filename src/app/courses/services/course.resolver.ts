ac3e44e07c11376c61a7f797283e41ffa1bbe8f3
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-28 13:08:35 +0200

    Router loading indicator - detect route transitions and lazy loading

--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,13 +1,12 @@
 import {
   ActivatedRouteSnapshot,
-  Resolve,
   ResolveFn,
   RouterStateSnapshot,
 } from '@angular/router';
 import { Course } from '../model/course';
 import { Observable } from 'rxjs';
 import { CoursesService } from './courses.service';
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 
 export const courseResolver: ResolveFn<Course> = (
   route: ActivatedRouteSnapshot,
@@ -16,17 +15,3 @@ export const courseResolver: ResolveFn<Course> = (
   const courseUrl = route.paramMap.get('courseUrl');
   return inject(CoursesService).loadCourseByUrl(courseUrl);
 };
-
-// @Injectable()
-// export class CourseResolver implements Resolve<Course> {
-//   constructor(private courses: CoursesService) {}
-
-//   resolve(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot
-//   ): Observable<Course> {
-//     const courseUrl = route.paramMap.get('courseUrl');
-
-//     return this.courses.loadCourseByUrl(courseUrl);
-//   }
-// }
--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,4 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  RouteConfigLoadEnd,
+  RouteConfigLoadStart,
+  Router,
+} from '@angular/router';
 import { LoadingService } from './loading.service';
 
 @Component({
@@ -8,8 +17,27 @@ import { LoadingService } from './loading.service';
 })
 export class LoadingComponent implements OnInit {
   @Input() routing: boolean = false;
+  @Input() detectRoutingOngoing = false;
 
-  constructor(public loadingService: LoadingService) {}
+  constructor(public loadingService: LoadingService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.detectRoutingOngoing) {
+      this.router.events.subscribe((event) => {
+        if (
+          event instanceof NavigationStart ||
+          event instanceof RouteConfigLoadStart
+        ) {
+          this.loadingService.loadingOn();
+        } else if (
+          event instanceof NavigationEnd ||
+          event instanceof NavigationError ||
+          event instanceof NavigationCancel ||
+          event instanceof RouteConfigLoadEnd
+        ) {
+          this.loadingService.loadingOff();
+        }
+      });
+    }
+  }
 }
